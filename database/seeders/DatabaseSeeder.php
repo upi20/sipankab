905fcd566a3f8aa7b93e1dbcd69b7a6a690115fc
Isep Lutpi Nur <53908272+upi20@users.noreply.github.com>
2023-05-31 21:51:36 +0700

    fix error seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,21 +21,16 @@ class DatabaseSeeder extends Seeder
         $this->call(PModelHasRolesTableSeeder::class);
         $this->call(PRoleHasPermissionsTableSeeder::class);
         $this->call(PRoleHasMenuTableSeeder::class);
-
-        $this->call(ImportKriteriaTableSeeder::class);
-        $this->call(KriteriaTableSeeder::class);
-
-        $this->call(ImportPendudukTableSeeder::class);
-        $this->call(PendudukTableSeeder::class);
-        $this->call(PendudukNilaiTableSeeder::class);
-
         $this->call(SessionsTableSeeder::class);
         $this->call(LogsTableSeeder::class);
+
         $this->call(ImportKecamatanTableSeeder::class);
         $this->call(KecamatanTableSeeder::class);
+
         $this->call(ImportTahapanTableSeeder::class);
-        $this->call(ImportCalonTableSeeder::class);
         $this->call(TahapanTableSeeder::class);
+
+        $this->call(ImportCalonTableSeeder::class);
         $this->call(CalonTableSeeder::class);
         $this->call(CalonNilaiTableSeeder::class);
     }
